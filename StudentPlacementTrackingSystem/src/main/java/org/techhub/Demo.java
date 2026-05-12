7117b6942b28c16d386027fca0949970ed889e3b
yemulyuvraj <163127578+yemulyuvraj@users.noreply.github.com>
2026-05-12 10:24:56 +0530

    start

---
+++ StudentPlacementTrackingSystem/src/main/java/org/techhub/Demo.java
@@ -0,0 +1,31 @@
+package org.techhub;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Demo
+ */
+@WebServlet("/Demo")
+public class Demo extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Hello");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
