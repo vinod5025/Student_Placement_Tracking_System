7d92f0322a12405d65b89f0cdbf270b859724a5a
yemulyuvraj <163127578+yemulyuvraj@users.noreply.github.com>
2026-05-12 15:41:55 +0530

    register student successfully by vinod

---
+++ StudentPlacementTrackingSystem/src/main/java/repository/StudentRegisterRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.AddStudentModel;
+
+public interface StudentRegisterRepository {
+	public boolean isAddStudent(AddStudentModel smodel);
+
+}
---
+++ StudentPlacementTrackingSystem/src/main/java/service/StudentRegisterService.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.AddStudentModel;
+
+public interface StudentRegisterService {
+	public boolean isAddStudent(AddStudentModel asmodel);
+
+}
