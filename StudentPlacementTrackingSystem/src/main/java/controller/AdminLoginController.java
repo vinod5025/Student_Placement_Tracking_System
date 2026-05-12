83451e54f6e3dd2418eb09488fc3fe83a9baba30
yemulyuvraj <163127578+yemulyuvraj@users.noreply.github.com>
2026-05-12 14:29:52 +0530

    afternoon file

--- StudentPlacementTrackingSystem/src/main/java/controller/AdminLoginController.java
+++ StudentPlacementTrackingSystem/src/main/java/controller/AdminLoginController.java
@@ -111,7 +111,6 @@ public class AdminLoginController extends HttpServlet {
 			}
 		}
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
